b7020d6e43bdd64ac568034c2c7ee153273adba7
wereneke <weronikad752@gmail.com>
2018-03-13 11:47:47 +0100

    refactor main

--- src/main/java/com/weratom/App.java
+++ src/main/java/com/weratom/App.java
@@ -11,24 +11,7 @@ public class App
     private static Scanner in = new Scanner(System.in);
     public static void main( String[] args ) {
 
-        Server server = new Server();
-        new Thread(server).start();
 
-        Client client = new Client("localhost");
-        boolean toRun = true;
-        while (toRun) {
-            try {
-                client.askServerForInfo();
-                Thread.sleep(1000);
-                System.out.println("exit? y/n");
-                toRun = (in.nextLine().toLowerCase().equals("y")) ? false : true;
-
-                if(!toRun) server.stop();
-
-            }catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
 
 
     }
